163f8ef65c1ed8328f99b35c4b58c65135a77802
Your Name <you@example.com>
2021-01-09 11:06:55 +0100

    entrainement

--- exercises/TD05_gui/app.py
+++ exercises/TD05_gui/app.py
@@ -1,15 +1,8 @@
-
 from tkinter import *
 
 # creer une premiere fenetre 
 window = Tk() 
 
-#personnaliser cette fenetre ouverte 
-window.title()
-window.geometry(600*600)
-
-
 
 #afficher 
 window.mainloop()
-
